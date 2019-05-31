c25ac8e982d936a1436d6c453760f7e87221f84c
John Wyatt <wyatt1117@yahoo.com>
2019-05-31 10:35:25 -0400

    Added a debugger statement to demonstrate debugging with Sourcemaps in the browser dev tools.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import "./index.css";
 import numeral from "numeral";//a good library for formatting numbers
 
 const courseValue = numeral(1000).format("$0,0.00");
+debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
