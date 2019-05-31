81058b5caec958addec3f85a5056754aa4ff30bc
John Wyatt <wyatt1117@yahoo.com>
2019-05-31 10:12:00 -0400

    Added an index.js file so webpack has something to bundle.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import numeral from "numeral";//a good library for formatting numbers
+
+const courseValue = numeral(1000).format("$0,0.00");
+console.log(`I would pay ${courseValue} for this awesome course!`);
