921c559d417c953eef39e5d4f563e34961d63ec3
John Wyatt <wyatt1117@yahoo.com>
2019-06-17 16:53:13 -0400

    Modified baseUrl.js to be more flexible in switching from prod to dev servers when developing by adding a useMockApi query string to the url.

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,13 @@
 export default function getBaseUrl() {
-	const inDevelopment = window.location.hostname === 'localhost';//checks to see if it's running in dev and sets the base url accordingly
-	return inDevelopment ? 'http://localhost:3001/' : '/';
+	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringParameterByName(name, url) {//necessary for getting a parameter from URL. He got it from StackOverflow
+	if(!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if(!results) return null;
+	if(!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
