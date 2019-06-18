adb293cdaa8f5769d84860b7abb106a969e891f2
John Wyatt <wyatt1117@yahoo.com>
2019-06-18 16:10:21 -0400

    Added the HtmlWebpackplugin to webpack config for prod so it'll create index.html in the dist directory when the prod build is ran. Also added bundling of 3rd party JS libraries to be separate from the JS application bundle.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,48 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackplugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map', // a little slower to build but recommended for production because it's high quality experience
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+		vendor: path.resolve(__dirname, 'src/vendor'),
+		main: path.resolve(__dirname, 'src/index'),
+
+	},
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),//popular convention and stands for distribution
     publicPath: '/',
-    filename: 'bundle.js'
+		filename: '[name].js'	//need a placeholder instead of the hardcoded bundle.js
+													//this tells webpack to use the name we defined in the entry point
+													//it will now generate a main.js and a vendor.js
+													//it will automatically write references to both files in the html file
   },
   plugins: [
+		// Use CommonsChunkPlugin to create a separate bundle
+		// of vendor libraries so that they're cached separately.
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor'
+		}),
+		//Create HTML file that includes reference to bundled JS
+		new HtmlWebpackplugin({
+			template: 'src/index.html',
+			inject: true,
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true
+			}
+		}),
 		// Eliminate duplicate packages when generating bundle
 		new webpack.optimize.DedupePlugin(),
 		// Minify JS
